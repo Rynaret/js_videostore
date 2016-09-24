eb6a605b2dabd87a0241b4aa3867b10b64d8ad13
Rynaret <sun.re@bk.ru>
2016-09-24 22:49:37 +0300

    Переименовал переменные

--- store.js
+++ store.js
@@ -1,36 +1,36 @@
-"use strict"
+"use strict";
 
 function statement(customer, movies) {
   let totalAmount = 0;
-  let frequentRenterPoints = 0;
+  let totalFrequentRenterPoints = 0;
   let result = `Rental Record for ${customer.name}\n`;
-  for (let r of customer.rentals) {
-    let movie = movies[r.movieID];
+  for (let rental of customer.rentals) {
+    let movie = movies[rental.movieID];
     let thisAmount = 0;
 
     // determine amount for each movie
     switch (movie.code) {
       case "regular":
         thisAmount = 2;
-        if (r.days > 2) {
-          thisAmount += (r.days - 2) * 1.5;
+        if (rental.days > 2) {
+          thisAmount += (rental.days - 2) * 1.5;
         }
         break;
       case "new":
-        thisAmount = r.days * 3;
+        thisAmount = rental.days * 3;
         break;
       case "childrens":
         thisAmount = 1.5;
-        if (r.days > 3) {
-          thisAmount += (r.days - 3) * 1.5;
+        if (rental.days > 3) {
+          thisAmount += (rental.days - 3) * 1.5;
         }
         break;
     }
 
     //add frequent renter points
-    frequentRenterPoints++;
+    totalFrequentRenterPoints++;
     // add bonus for a two day new release rental
-    if (movie.code === "new" && r.days > 2) frequentRenterPoints++;
+    if (movie.code === "new" && rental.days > 2) totalFrequentRenterPoints++;
 
     //print figures for this rental
     result += `\t${movie.title}\t${thisAmount}\n`;
@@ -38,7 +38,7 @@ function statement(customer, movies) {
   }
   // add footer lines
   result += `Amount owed is ${totalAmount}\n`;
-  result += `You earned ${frequentRenterPoints} frequent renter points\n`;
+  result += `You earned ${totalFrequentRenterPoints} frequent renter points\n`;
 
   return result;
 }
@@ -52,7 +52,7 @@ let customer = {
     "movieID": "F002",
     "days": 1
   }, ]
-}
+};
 
 let movies = {
   "F001": {
@@ -64,6 +64,6 @@ let movies = {
     "code": "regular"
   },
   // etc
-}
+};
 
-console.log(statement(customer, movies))
+console.log(statement(customer, movies));
